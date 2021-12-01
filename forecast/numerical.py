aa05769a6b5a41a70aa5c88ce0d464a654aac460
dkhyun01 <dkhyunji19@gmail.com>
2021-12-01 23:26:50 +0900

    add remark of adaboost model

--- forecast/numerical.py
+++ forecast/numerical.py
@@ -14,3 +14,28 @@ def multi_regression(info)->float:
     """
     value = -6120704+(1073*info['TVD (ft)'])+(2762194*info['Bot-Hole Northing (NAD83)'])+(29.4*info['Total Proppant Placed (tonne)'])-(17.6*info['Total Sand Proppant Placed (tonne)'])-(0.0465*info['TVD (ft)']*info['TVD (ft)'])-(147336*info['Bot-Hole Northing (NAD83)']*info['Bot-Hole Northing (NAD83)'])+(0.001349*info['Total Proppant Placed (tonne)']*info['Total Proppant Placed (tonne)'])-(248.3*info['TVD (ft)']*info['Bot-Hole Northing (NAD83)'])-(36.02*info['Bot-Hole Northing (NAD83)']*info['Total Sand Proppant Placed (tonne)'])
     return value
+
+
+'''
+#hyunji
+
+def adaboost(info)->int:
+    x = df.drop(['First 6 mo. Avg. GAS (Mcf)'], axis=1)
+    y = df['First 6 mo. Avg. GAS (Mcf)']
+ 
+    # Split dataset into training set and test set
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3) # 70% training and 30% test
+ 
+    # Create adaboost classifer object
+    #base_model = DecisionTreeClassifier(max_depth = 5)
+    abr_model = AdaBoostRegressor(n_estimators=50,
+                                learning_rate=0.1)
+    # Train Adaboost Classifer
+    abr_model.fit(x_train, y_train)
+
+    # 모델 검증 (예측의 결정계수 반환)
+    print(abr_model.score(x_train, y_train))
+    print(abr_model.score(x_test, y_test))
+
+    return  
+'''
