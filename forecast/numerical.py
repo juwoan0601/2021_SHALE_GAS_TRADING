6ea630820ffdd17ec8e015d431acb3ebd6d41dd6
JuwanHan <juwoan0601@gmail.com>
2021-12-01 23:26:13 +0900

    Add forecaset function

--- forecast/numerical.py
+++ forecast/numerical.py
@@ -14,3 +14,9 @@ def multi_regression(info)->float:
     """
     value = -6120704+(1073*info['TVD (ft)'])+(2762194*info['Bot-Hole Northing (NAD83)'])+(29.4*info['Total Proppant Placed (tonne)'])-(17.6*info['Total Sand Proppant Placed (tonne)'])-(0.0465*info['TVD (ft)']*info['TVD (ft)'])-(147336*info['Bot-Hole Northing (NAD83)']*info['Bot-Hole Northing (NAD83)'])+(0.001349*info['Total Proppant Placed (tonne)']*info['Total Proppant Placed (tonne)'])-(248.3*info['TVD (ft)']*info['Bot-Hole Northing (NAD83)'])-(36.02*info['Bot-Hole Northing (NAD83)']*info['Total Sand Proppant Placed (tonne)'])
     return value
+
+def multi_regression_last6(info)->float: # JUWAN
+    """ Multi regression Model with minitab (Last 6 month)
+    """
+    value =-369503+(43.9*info['MD (All Wells) (ft)'])+(130.5*info['Avg Frac Spacing (m)'])+(20.32*info['Completed Length (m)'])-(781*info['Bot-Hole direction (N/S)/(E/W)'])-(0.001363*info['MD (All Wells) (ft)']*info['MD (All Wells) (ft)'])+(13.95*info['Avg Frac Spacing (m)']*info['Bot-Hole direction (N/S)/(E/W)'])
+    return value
